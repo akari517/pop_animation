d87867ad854566feb5dc5d54d550ae76270748fd
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-09-25 08:35:33 +0900

    add main.py

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+import os
+import logging
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+# Define the response model for the root endpoint
+class HelloResponse(BaseModel):
+    message: str
+
+
+# Initialize the FastAPI app
+app = FastAPI()
+
+# Configure logging for the app
+logger = logging.getLogger("uvicorn")
+logger.level = logging.INFO
+
+# Configure CORS to allow requests from the frontend
+origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
+# Define the root endpoint
+@app.get("/", response_model=HelloResponse)
+def hello():
+    return HelloResponse(**{"message": "Hello, world!"})
