1b78821679ea13e0f5ab797588618f2c8e1b030b
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-09-27 11:01:27 +0900

    add db connection func

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
+import psycopg2
 
 # 環境変数読み込み
 load_dotenv()
-DATABASE_URL = os.getenv("DB_EXTERNAL_URL")
+# TODO: RenderにデプロイするときはおそらくDB_INTERNAL_URLに変えたほうがいい？
+DATABASE_URL = os.getenv("DB_EXTERNAL_URL") or os.getenv("DATABASE_URL")
 
 # ルートエンドポイントのレスポンスモデル
 class HelloResponse(BaseModel):
     message: str
 
 # DB保存リクエスト
+# TODO: schema決まったら修正
 class ImageRecordRequest(BaseModel):
     s3_url: str
     user_id: int
@@ -36,13 +39,40 @@ app.add_middleware(
 
 def get_db_connection():
     """
-    新しい psycopg2 接続を返す。呼び出し側で close() してください。
-    簡易実装のため接続プールは使っていません。
+    新しい psycopg2 接続を返す。呼び出し側で必ず close() 必要。
+    withで呼び出すなら自動で閉じるのでcloseは不要
     """
     db_url = DATABASE_URL
     if not db_url:
         raise RuntimeError("環境変数 DB_EXTERNAL_URL または DATABASE_URL が設定されていません。")
     return psycopg2.connect(db_url)
+
+
+
+def get_all_images_from_db():
+    """
+    DBから全件取得する関数。
+    """
+    conn = get_db_connection()
+    
+    # TODO: クエリは修正
+    query = """
+    SELECT * FROM XXX;
+    """
+    try:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            rows = cur.fetchall()
+            # TODO: schema決まったら修正
+            images_list = [{"name": name, "category": category, "image_name": image_name} for name, category, image_name in rows]
+            result = {"images": images_list}
+            return result
+    finally:
+        conn.close()
+    
+    
+
+
 
 # ルートエンドポイント
 @app.get("/", response_model=HelloResponse)
@@ -53,14 +83,9 @@ def hello():
 # 画像一覧取得エンドポイント
 @app.get("/images")
 def get_all_images():
-    # DBから全件取得
-    # conn = get_db_connection()
-    
-    return {
-        "images": []
-    }
-    
-    
+    return get_all_images_from_db()
+
+
 # 画像取得エンドポイント
 @app.get("/images/{image_id}")
 def get_image_by_id(image_id: int):
