cc73e4567f73f80d5f3f74aaf76ba1605f7a1d7c
akari517 <snow.aa.goo3@gmail.com>
2025-09-28 07:10:17 +0900

    落書き画面土台完成

--- src/screens/animation/SketchScreen.jsx
+++ src/screens/animation/SketchScreen.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function SkechScreen() {
+function SketchScreen() {
   return (
     <div className="screen-container">
-      <h1>お気に入り画面</h1>
+      <h1>落書き画面</h1>
       <p>この画面は現在準備中です。</p>
     </div>
   );
 }
 
-export default SkechScreen;
+export default SketchScreen;
