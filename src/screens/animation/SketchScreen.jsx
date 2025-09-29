9f4e092b0bdc4f3c46ad11e862f3ea07fe76306d
akari517 <snow.aa.goo3@gmail.com>
2025-09-29 23:12:26 +0900

    参考用

--- src/screens/animation/SketchScreen.jsx
+++ src/screens/animation/SketchScreen.jsx
@@ -1,70 +1,106 @@
 import React from "react";
-import { Stage, Layer, Line, Text } from 'react-konva';
+import { Stage, Layer, Line,Image,Arc } from 'react-konva';
 import useImage from "use-image";
+
+import React, { useEffect, useRef, useState } from "react";
 //画像のpath
 //import sampleImage from "../assets/image4.jpg";
-import sampleImage from "../../assets/image4.jpg";
+import image2 from "../../assets/image4.jpg";  
 
-function SketchScreen() {
-  const [image] = useImage(sampleImage);// ここで画像を読み込む
-    console.log("image", image);
-  const [lines, setLines] = React.useState([]);
+
+const URLImage = ({ src, ...rest }) => {
+  const [image] = useImage(src, "anonymous");
+  return (
+    <Image
+      image={image}
+      width={((image?.width ?? 1) / (image?.height ?? 1)) * 500}
+      height={500}
+      {...rest}
+    />
+  );
+};
+
+const SketchScreen = () => {
   const isDrawing = React.useRef(false);
+  const [lines, setLines] = useState([]);
 
+  // 背景画像のロード処理
+  // const [backgroundImage, setBackgroundImage] = useState<HTMLImageElement>();
+  // useEffect(() => {
+  //   const image = new window.Image();
+  //   image.src = "/img/sampledeno.jpeg";
+  //   image.onload = () => {
+  //     setBackgroundImage(image);
+  //   };
+  // }, []);
+
+  // クリックした位置からペイントスタート
   const handleMouseDown = (e) => {
+    const point = e.target.getStage()?.getPointerPosition();
+    if (!point) return;
     isDrawing.current = true;
-    const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { points: [pos.x, pos.y] }]);
+    // スタート位置をセット
+    setLines([...lines, [point.x, point.y]]);
   };
 
+  // マウスドラッグ中は現在地をセットし続ける
   const handleMouseMove = (e) => {
-    if (!isDrawing.current) return;
-    const stage = e.target.getStage();
-    const point = stage.getPointerPosition();
-    const lastLine = lines[lines.length - 1];
-    // 新しい点を追加
-    lastLine.points = lastLine.points.concat([point.x, point.y]);
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.concat());
+    const point = e.target.getStage()?.getPointerPosition();
+    if (!point || !isDrawing.current) return;
+
+    const lastIdx = lines.length - 1;
+    setLines((prev) => [
+      ...prev.slice(0, lastIdx),
+      [...prev[lastIdx], point.x, point.y],
+    ]);
   };
 
+  // 離したタイミングでペイント終わる
   const handleMouseUp = () => {
     isDrawing.current = false;
   };
-  
-  console.log();
+
   return (
-    <div className="screen-container">
-    <h1>ようこそ！</h1>
     <>
-  
-    </>
-    <div style={{ width: "300", height: "200" }}>
-      <Stage
-        width={500}
-        height={500}
-        onMouseDown={handleMouseDown}
-        onMousemove={handleMouseMove}
-        onMouseup={handleMouseUp}
+      <Button onClick={() => setLines([])}>all clear</Button>
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          border: "solid #dee2e6",
+          backgroundColor: "#FFEEF0",
+        }}
       >
-        <Layer>
-          {image && <Image image={image} x={0} y={100} width={300} height={200} />}
-          {lines.map((line, i) => (
-            <Line
-              key={i}
-              points={line.points}
-              stroke="red"
-              strokeWidth={5}
-              tension={0.5}
-              lineCap="round"
-              lineJoin="round"
-            />
-          ))}
-        </Layer>
-      </Stage>
+        <Stage
+          width={500}
+          height={500}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          {/* Layerを分けるとお絵描き中、背景Img側Layerは再レンダリングされない */}
+          <Layer>
+            <URLImage src="/img/sampledeno.png" />
+            {/* <Image
+              image={backgroundImage}
+              // アスペクト比維持
+              width={
+                ((backgroundImage?.width ?? 1) /
+                  (backgroundImage?.height ?? 1)) *
+                500
+              }
+              height={500}
+            /> */}
+          </Layer>
+          <Layer>
+            {lines.map((line, i) => (
+              <Line key={i} points={line} stroke="black" />
+            ))}
+          </Layer>
+        </Stage>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default SketchScreen;
