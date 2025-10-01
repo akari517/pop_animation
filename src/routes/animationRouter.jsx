691e349cf399eaecc737eb3d7db41962a8d1c1aa
akari517 <snow.aa.goo3@gmail.com>
2025-10-02 06:18:52 +0900

    遷移完了途中

---
+++ src/routes/animationRouter.jsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import AnimationHomeScreen from "../screens/animation/AnimationHomeScreen";
+import SkechScreen from "../screens/animation/SkechScreen";   
---
+++ src/screens/animation/AnimationHomeScreen.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import { Box, BottomNavigation, BottomNavigationAction, Button } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import AddIcon from "@mui/icons-material/Add";
+import { RouterProvider } from "react-router-dom";
+import {router} from "../../routes/router";
+
+const AnimationHomeScreen = ()=>{
+  return (
+    <Box>
+        <h1>Animation Home Screen</h1>
+    </Box>
+
+  );
+}
+
+export default AuthScreen;
+
+
+
