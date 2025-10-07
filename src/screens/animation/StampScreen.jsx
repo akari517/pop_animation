f23a6394a51884ada5190d835720a04938753cd7
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-10-07 20:18:09 +0900

    add gif file and setup base code

--- src/screens/animation/StampScreen.jsx
+++ src/screens/animation/StampScreen.jsx
@@ -1,18 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Stage, Layer, Line } from "react-konva";
+import "./FrameMotionScreen.css";
+import image2 from "../../assets/image4.jpg";
+import URLImage from "../../components/URLImage.jsx";
+import { useStageSize } from "../../components/useStageSize.jsx";
+import { useDrawing } from "../../components/useDrawing";
 
-function  StampScreen() {
+function FrameMotionScreen() {
+  const stageSize = useStageSize();
+  const [tick, setTick] = useState(0);
+  const [activeAnimation, setActiveAnimation] = useState(null); // null, "wave", "pulse", "fade"
+
+  const {
+    handleDown,
+    handleMove,
+    endDrawing,
+    undo,
+    redo,
+    clear
+  } = useDrawing([], "#0ff", "pen");
+
+
   return (
-    <div className="screen-container">
-      <h1>ようこそ！</h1>
-      <Link to="/login" className="button">
-        ログイン
-      </Link>
-      <Link to="/register" className="button">
-        新規登録
-      </Link>
+    <div className="frame-container">
+
+      <Stage
+        width={stageSize.width}
+        height={stageSize.height}
+        onMouseDown={handleDown}
+        onMouseMove={handleMove}
+        onMouseUp={endDrawing}
+        onTouchStart={handleDown}
+        onTouchMove={handleMove}
+        onTouchEnd={endDrawing}
+      >
+        {/* 背景画像 */}
+        <Layer>
+          <URLImage src={image2} stageWidth={stageSize.width} stageHeight={stageSize.height} />
+        </Layer>
+
+      </Stage>
     </div>
   );
 }
 
-export default StampScreen;
+export default FrameMotionScreen;
